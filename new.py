87fbac02f0d51579e7eaaef477f813015685d745
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-08-25 15:47:05 +0530

    flask

--- new.py
+++ new.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 web=Flask(__name__)
 
@@ -7,6 +7,14 @@ web=Flask(__name__)
 
 def home():
     return render_template('register.html')
+
+@web.route('/confirmation',methods=['GET','POST'])
+def register():
+    if request.method=='POST':
+        name=request.form.get('name')
+        city=request.form.get('city')
+        p=request.form.get('phone number')
+        return render_template('confirm.html',name=name,city=city,phone_number=p)
 
 if __name__ == '__main__':
     web.run(debug=True)
