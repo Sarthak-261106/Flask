26b249b0781a21b234cbf110f1ffe882c685a755
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-08-25 15:24:33 +0530

    Update new.py

--- new.py
+++ new.py
@@ -3,10 +3,12 @@ from flask import Flask
 web=Flask(__name__)
 
 @web.route('/')
-
+@web.route('/home')
 
 def home():
     return "Welcome"
 
 if __name__ == '__main__':
     web.run(debug=True)
+
+    
